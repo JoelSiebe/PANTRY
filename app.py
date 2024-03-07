2d1bc331df65154d496cf378c87470f071fad8be
Joël Sieber <162429161+JoelSiebe@users.noreply.github.com>
2024-03-07 23:57:37 +0100

    s

--- app.py
+++ app.py
@@ -4,5 +4,5 @@ import streamlit as st
 st.title("Pantry Pal - Conquering Leftovers, Mastering Meals")
 st.header("**Tame your kitchen with Pantry Pal**")
 
-image = Image.open("C:\\Users\\joels\\OneDrive\\Privat\\Bilder\\Pictures\\1.jpg")
+image = Image.open("C:\\Users\\joels\\OneDrive - Universitaet St.Gallen\\Mastervorbereitungsstufe\\CS\\App\\PANTRY\\1.jpg")
 st.image(image, caption='Hi!', use_column_width=True)
