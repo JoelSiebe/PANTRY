e990acb3a2ca1652c7f65d17aab32f4dc41c9e06
Joël Sieber <162429161+JoelSiebe@users.noreply.github.com>
2024-03-08 00:29:49 +0100

    s

--- app.py
+++ app.py
@@ -1,20 +1,17 @@
 import streamlit as st
 
-page_bg_img = f"""
+# Definiere das Hintergrundbild und den Stil
+page_bg_img = """
 <style>
-[data-testid="stAppViewContainer"] > .main {{
+body {
 background-image: url("https://i.postimg.cc/P5ksXpm2/pexels-vladimir-gladkov-6208084.jpg");
 background-size: cover;
-background-position: center center;
-background-repeat: no-repeat;
-background-attachment: local;
-}}
-[data-testid="stHeader"] {{
-background: rgba(0,0,0,0);
-}}
+}
+.stApp {
+color: white;
+}
 </style>
 """
-
 st.markdown(page_bg_img, unsafe_allow_html=True)
 
 # Titel und Header der Anwendung
@@ -22,16 +19,14 @@ st.title("Pantry Pal - Conquering Leftovers, Mastering Meals")
 st.header("**Tame your kitchen with Pantry Pal**")
 
 # Eingabefeld für die Kühlschrank-Zutaten
-ingredients = st.text_input("Geben Sie Ihre Kühlschrank-Zutaten ein, getrennt durch Komma")
+ingredients = st.text_input("Enter your fridge ingredients, separated by comma")
 
 # Schaltfläche, um Rezepte basierend auf den eingegebenen Zutaten anzuzeigen
-# if st.button("Rezepte anzeigen"):
-    # Hier können Sie die Logik zum Abrufen von Rezepten basierend auf den eingegebenen Zutaten implementieren
-    # Dies umfasst die Integration der Fooddatabank-API und der Rezept-API
-    # Sobald Sie Zugriff auf diese APIs haben, können Sie die entsprechenden Anfragen senden und die Ergebnisse anzeigen
+if st.button("Show Recipes"):
+    # Hier kannst du die Logik zum Abrufen von Rezepten basierend auf den eingegebenen Zutaten implementieren
     # In diesem Beispiel zeigen wir nur eine Platzhaltermeldung an
-    #st.write("Hier werden die Rezepte basierend auf Ihren Zutaten angezeigt")
+    st.write("Here, we will display recipes based on your ingredients")
 
 # Fußzeile der Anwendung
 st.markdown("---")
-st.write("© 2024 Pantry Pal. Alle Rechte vorbehalten.")
+st.write("© 2024 Pantry Pal. All rights reserved.")
