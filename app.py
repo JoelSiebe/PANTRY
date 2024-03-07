ae31fe046775843530b434d72189e1a5da19661b
Joël Sieber <162429161+JoelSiebe@users.noreply.github.com>
2024-03-08 00:12:27 +0100

    s

--- app.py
+++ app.py
@@ -1,17 +1,16 @@
 import streamlit as st
 
-page_bg_img = f"""
+page_bg_img = """
 <style>
-[data-testid="stAppViewContainer"] > .main {{
+body {
 background-image: url("https://i.postimg.cc/L6kcC417/1.jpg");
 background-size: cover;
-background-position: center center;
-background-repeat: no-repeat;
-background-attachment: local;
-}}
-[data-testid="stHeader"] {{
-background: rgba(0,0,0,0);
-}}
+}
+.page {
+background-color: rgba(255,255,255,0.8);
+padding: 20px;
+border-radius: 5px;
+}
 </style>
 """
 
@@ -21,16 +20,22 @@ st.markdown(page_bg_img, unsafe_allow_html=True)
 st.title("Pantry Pal - Conquering Leftovers, Mastering Meals")
 st.header("**Tame your kitchen with Pantry Pal**")
 
+# Zentrierte Ausrichtung des Inhalts
+st.markdown('<p class="page">')
+
 # Eingabefeld für die Kühlschrank-Zutaten
 ingredients = st.text_input("Geben Sie Ihre Kühlschrank-Zutaten ein, getrennt durch Komma")
 
 # Schaltfläche, um Rezepte basierend auf den eingegebenen Zutaten anzuzeigen
 # if st.button("Rezepte anzeigen"):
-    # Hier können Sie die Logik zum Abrufen von Rezepten basierend auf den eingegebenen Zutaten implementieren
-    # Dies umfasst die Integration der Fooddatabank-API und der Rezept-API
-    # Sobald Sie Zugriff auf diese APIs haben, können Sie die entsprechenden Anfragen senden und die Ergebnisse anzeigen
-    # In diesem Beispiel zeigen wir nur eine Platzhaltermeldung an
-    #st.write("Hier werden die Rezepte basierend auf Ihren Zutaten angezeigt")
+# Hier können Sie die Logik zum Abrufen von Rezepten basierend auf den eingegebenen Zutaten implementieren
+# Dies umfasst die Integration der Fooddatabank-API und der Rezept-API
+# Sobald Sie Zugriff auf diese APIs haben, können Sie die entsprechenden Anfragen senden und die Ergebnisse anzeigen
+# In diesem Beispiel zeigen wir nur eine Platzhaltermeldung an
+# st.write("Hier werden die Rezepte basierend auf Ihren Zutaten angezeigt")
+
+# Schließen Sie die zentrierte Ausrichtung des Inhalts
+st.markdown('</p>')
 
 # Fußzeile der Anwendung
 st.markdown("---")
