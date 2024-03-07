78fc639f90b09482ea78568c81e9b6f7b0565ac5
Joël Sieber <162429161+JoelSiebe@users.noreply.github.com>
2024-03-08 00:09:42 +0100

    s

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import streamlit as st
 page_bg_img = f"""
 <style>
 [data-testid="stAppViewContainer"] > .main {{
-background-image: url("https://i.postimg.cc/4xgNnkfX/Untitled-design.png");
+background-image: url("https://i.postimg.cc/L6kcC417/1.jpg");
 background-size: cover;
 background-position: center center;
 background-repeat: no-repeat;
