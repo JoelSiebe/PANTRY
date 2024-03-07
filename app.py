a9ef457af6c2b298cb2fb005ea7d00f70e9257e5
Joël Sieber <162429161+JoelSiebe@users.noreply.github.com>
2024-03-07 23:53:02 +0100

    s

--- app.py
+++ app.py
@@ -1,25 +1,9 @@
+from PIL import Image
 import streamlit as st
-import base64
 
 st.title("Pantry Pal - Conquering Leftovers, Mastering Meals")
 st.header("**Tame your kitchen with Pantry Pal**")
 
-# Bilddateien in Bytes umwandeln
-def img_to_bytes(img_path):
-    with open(img_path, "rb") as f:
-        img_bytes = f.read()
-    encoded = base64.b64encode(img_bytes).decode()
-    return encoded
-
-# HTML-Code für das Bild generieren
-def img_to_html(img_path):
-    img_html = "<img src='data:image/png;base64,{}' class='img-fluid'>".format(
-        img_to_bytes(img_path)
-    )
-    return img_html
-
-# Hier setzen Sie den Pfad zu Ihrer lokalen Bilddatei ein
 image_path = 'C:\Users\joels\OneDrive\Privat\Bilder\Pictures\pexels-jane-doan-1099680.jpg'
-
-# Das Bild in Markdown einfügen, unsafe_allow_html=True ermöglicht die Verwendung von HTML im Markdown
-st.markdown(img_to_html(image_path), unsafe_allow_html=True)
+image = Image.open(image_path)
+st.image(image, caption='Bildunterschrift hier', use_column_width=True)
