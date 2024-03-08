9639a15254d13687662c3b611dd11fbbc1d73c1c
Joël Sieber <162429161+JoelSiebe@users.noreply.github.com>
2024-03-08 04:19:21 +0100

    wieder ohne sidebar

--- app.py
+++ app.py
@@ -11,6 +11,30 @@ css_styles = """
     background-repeat: no-repeat;
     background-attachment: local;
 }
+
+[data-testid="stHeader"] {
+    background: rgba(0,0,0,0);
+}
+
+.text-input-container {
+    background-color: rgba(200, 200, 200, 0.8) !important;
+    border-radius: 20px !important;
+    padding: 20px !important;
+}
+
+.text-input-container input {
+    color: black !important; /* Schriftfarbe auf Schwarz */
+    font-size: 24px !important; /* Schriftgröße auf 24px */
+    background-color: rgba(150, 150, 150, 0.9) !important; /* Hintergrundfarbe mit Transparenz */
+    border: none !important; /* Rand entfernen */
+    border-radius: 15px !important; /* Randradius festlegen */
+    padding: 15px 20px !important; /* Innenabstand anpassen */
+}
+
+h2 {
+    font-size: 36px;
+    color: white;
+}
 </style>
 """
 
@@ -20,21 +44,11 @@ st.markdown(css_styles, unsafe_allow_html=True)
 st.title("Pantry Pal - Conquering Leftovers, Mastering Meals",)
 st.header("**Tame your kitchen with Pantry Pal**",)
 
-# Boolescher Wert, um zu überprüfen, ob die Sidebar angezeigt wird
-show_sidebar = st.sidebar.checkbox("Show Sidebar")
+# Zutatenliste des Benutzers als Eingabefeld
+zutaten = st.text_input("Enter what's left in your fridge (separated by comma)", key="ingredients", max_chars=1000)
 
-# Wenn die Sidebar angezeigt wird
-if show_sidebar:
-    st.sidebar.header('Filter Options')
-    # Weitere Filteroptionen hier einfügen
-
-# Eingabefeld für Zutatenliste
-if not show_sidebar:
-    zutaten = st.text_input("Enter what's left in your fridge (separated by comma)")
-
-# Button, um Filter anzuwenden und Rezepte anzuzeigen
-if not show_sidebar or st.button('Show recipes'):
-    if not show_sidebar or zutaten:
+if st.button('Show recipes'):
+    if zutaten:
         # Spoonacular API-URL
         api_url = "https://api.spoonacular.com/recipes/findByIngredients"
 
@@ -52,7 +66,17 @@ if not show_sidebar or st.button('Show recipes'):
         response = requests.get(api_url, params=params)
         data = response.json()
 
-        #Rezeptvorschläge 
+        # Neue Seite für Rezepte
+        st.markdown('<a href="#recipes">Go to recipes</a>', unsafe_allow_html=True)
+
+# Fußzeile der Anwendung
+st.markdown("---")
+st.write("© 2024 Pantry Pal. All rights reserved.")
+
+# Rezepte anzeigen
+if zutaten and st.button('Show recipes'):
+    if zutaten:
+        st.markdown('<a name="recipes"></a>', unsafe_allow_html=True)
         st.header("Look what we've found for you")
         for recipe in data:
             st.subheader(recipe['title'])
@@ -61,7 +85,3 @@ if not show_sidebar or st.button('Show recipes'):
             st.write(f"Fehlende Zutaten: {', '.join([ingredient['name'] for ingredient in recipe['missedIngredients']])}")
             st.write(f"Anzahl der fehlenden Zutaten: {recipe['missedIngredientCount']}")
             st.write(f"Anzahl der verwendeten Zutaten: {recipe['usedIngredientCount']}")
-
-# Fußzeile der Anwendung
-st.markdown("---")
-st.write("© 2024 Pantry Pal. All rights reserved.")
