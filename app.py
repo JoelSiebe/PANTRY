289f9138cb422cccc7441e236d3486ba7739a5d6
Joël Sieber <162429161+JoelSiebe@users.noreply.github.com>
2024-03-08 04:23:22 +0100

    filterparamet nun unter header

--- app.py
+++ app.py
@@ -23,22 +23,44 @@ st.header("**Tame your kitchen with Pantry Pal**",)
 # Zutatenliste des Benutzers als Eingabefeld
 zutaten = st.text_input("Enter what's left in your fridge (separated by comma)", key="ingredients", max_chars=1000)
 
+# Filteroptionen
+schwierigkeitsgrad = st.selectbox("Select Difficulty", ["Any", "Easy", "Medium", "Hard"])
+zeitdauer = st.selectbox("Select Cooking Time", ["Any", "0-15 minutes", "15-30 minutes", "30-60 minutes", "60+ minutes"])
+anzahl_zutaten = st.slider("Number of Ingredients", min_value=1, max_value=20, value=5)
+
+# Button, um Rezepte anzuzeigen
 if st.button('Show recipes'):
     if zutaten:
         # Spoonacular API-URL
         api_url = "https://api.spoonacular.com/recipes/findByIngredients"
 
-        #API-Schlüssel
+        # API-Schlüssel
         api_key = "06491aabe3d2435b8b21a749de46b765"
 
-        #Datenbankabfrage
+        # Datenbankabfrage
         params = {
             'ingredients': zutaten,
-            'number': 5, #Anz. angezeiter Rezepte
+            'number': 5,  # Anz. angezeiter Rezepte
             'apiKey': api_key
         }
 
-        #API-Abfrage senden
+        # Hinzufügen der Filterparameter
+        if schwierigkeitsgrad != "Any":
+            params['difficulty'] = schwierigkeitsgrad.lower()
+        if zeitdauer != "Any":
+            if zeitdauer == "0-15 minutes":
+                params['maxReadyTime'] = 15
+            elif zeitdauer == "15-30 minutes":
+                params['maxReadyTime'] = 30
+            elif zeitdauer == "30-60 minutes":
+                params['maxReadyTime'] = 60
+            else:
+                params['maxReadyTime'] = 60  # 60+ minutes
+
+        if anzahl_zutaten:
+            params['number'] = anzahl_zutaten
+
+        # API-Abfrage senden
         response = requests.get(api_url, params=params)
         data = response.json()
 
