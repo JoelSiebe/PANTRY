ed714c889d6e7823415a2bb76e0267497ab07c7a
Joël Sieber <162429161+JoelSiebe@users.noreply.github.com>
2024-03-12 11:40:45 +0100

    s

--- app.py
+++ app.py
@@ -29,6 +29,16 @@ st.header("**Tame your kitchen with Pantry Pal**",)
 # Versch. Zutaten des Benutzers als Eingabefeld
 zutaten = st.text_input("Enter what's left in your fridge (separated by comma)")
 
+#popover (https://docs.streamlit.io/library/api-reference/layout/st.popover)
+popover = st.popover("Filter items")
+red = popover.checkbox("Show red items.", True)
+blue = popover.checkbox("Show blue items.", True)
+
+if red:
+    st.write(":red[This is a red item.]")
+if blue:
+    st.write(":blue[This is a blue item.]")
+
 if st.button('Show recipes'):
     if zutaten:
 
