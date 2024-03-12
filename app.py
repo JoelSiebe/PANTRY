3b27b084e7817648f27398bb2c12c230d0db07b4
Joël Sieber <162429161+JoelSiebe@users.noreply.github.com>
2024-03-12 14:36:22 +0100

    s

--- app.py
+++ app.py
@@ -142,19 +142,18 @@ if st.button('Show recipes'):
         for recipe in data:
             st.subheader(recipe['title'])
             st.image(recipe['image'])
-            st.write(f"Verwendete Zutaten: {', '.join([ingredient['name'] for ingredient in recipe['usedIngredients']])}")
-            st.write(f"Fehlende Zutaten: {', '.join([ingredient['name'] for ingredient in recipe['missedIngredients']])}")
-            st.write(f"Anzahl der fehlenden Zutaten: {recipe['missedIngredientCount']}")
-            st.write(f"Anzahl der verwendeten Zutaten: {recipe['usedIngredientCount']}")
+            st.write(f"Used ingredients: {', '.join([ingredient['name'] for ingredient in recipe['usedIngredients']])}")
+            st.write(f"Missing ingredients: {', '.join([ingredient['name'] for ingredient in recipe['missedIngredients']])}")
+            st.write(f"Number of missing ingredients: {recipe['missedIngredientCount']}")
+            st.write(f"Number of used ingredients: {recipe['usedIngredientCount']}")
   
 
             #Spoonacular-API für Rezeptinformationen (https://spoonacular.com/food-api/docs#Get-Recipe-Information) / Key ist derselbe
             api_informations_url = "https://api.spoonacular.com/recipes/{id}/information"
             
-            if 'id' in recipe:  #API prüfen, ob Zubereitungschritte verfügubar
+            if 'id' in recipe:  #API prüfen, ob Zubereitungschritte verfügubar 
                 recipe_id = recipe['id']
-                instructions_url = f"https://api.spoonacular.com/recipes/{recipe_id}/information"
-                instructions_response = requests.get(instructions_url, params={'apiKey': api_key})
+                instructions_response = requests.get(api_informations_url, params={'apiKey': api_key})
                 instructions_data = instructions_response.json()
 
                 if 'instructions' in instructions_data:
