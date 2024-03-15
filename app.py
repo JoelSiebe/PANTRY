f07723c89e4f5f7d118c07809db6d646151a45f4
Joël Sieber <162429161+JoelSiebe@users.noreply.github.com>
2024-03-15 20:31:32 +0100

    s

--- app.py
+++ app.py
@@ -3,7 +3,10 @@ import requests
 import numpy as np
 import pandas as pd
 
-# #CSS-Stil (https://discuss.streamlit.io/t/upload-background-image/59732 // https://www.w3schools.com/cssref/pr_background-image.php)
+# Übersicht über die verwendeten Namen:
+
+##Mit nachfolgendem Abschnitt kann ein Hintergrundbild eingefügt werden; 
+##CSS-Stil (https://discuss.streamlit.io/t/upload-background-image/59732 // https://www.w3schools.com/cssref/pr_background-image.php)
 # css_background = """   
 # <style>
 # [data-testid="stAppViewContainer"] > .main {
@@ -47,40 +50,13 @@ with col2:
 #    st.header("Or maybe Korean?")
    st.image("https://i.postimg.cc/RZ0FH4BX/pexels-valeria-boltneva-1199957.jpg")
 
+# weitere Untertitel -> noch schauen, ob mit CSS schöner gemacht werden kann.
+
 st.header("How does it goes?") 
 st.header("Firstly, enter what's left in your fride. Selcect any filters if needed.")
 st.title("Then let the magic begin")
 
 #Filteroptionen (https://docs.streamlit.io/library/api-reference/widgets)
-
-# Clickboxen, die temp. mit session_gate gespeichert werden (quelle (etw. abgeändert):https://stackoverflow.com/questions/71242486/how-to-make-n-checkboxes-in-streamlit)
-
-# # 1. Create a variable to store todos.
-# if not 'cuisine_list' in st.session_state:
-#     st.session_state.cuisine_list = []
-
-# # 2. Prompt the user in the form
-# with st.form(key='cuisine_form'):
-#     cuisine = st.text_input(label='Select your favorite cuisines')
-#     is_submit = st.form_submit_button('submit')
-
-# # 3. Store todo in todolist when submit button is hit.
-# if is_submit:
-#     st.session_state.cuisine_list.append(cuisine)
-    
-# # 4. Display the contents of todolist
-# with st.expander(label='List of selected cuisines', expanded=True):
-#     for i, cuisine_text in enumerate(st.session_state.cuisine_list):
-#         st.checkbox(label=f'{cuisine_text}', key=i)
-
-# cuisines_api = ['African', 'Asian' 'American', 'British', 'Caribbean', 'Chinese', 'Eastern European', 'European', 'French', 'German', 'Greek', 'Indian', 'Italian', 'Japanese', 'Mexican', 'Thai', 'Vietnamese',]
-
-# if not 'cuisine_list' in st.session_state:
-#     st.session_state.cuisine_list = [False]*len(cuisines_api)
-
-# with st.expander(label='Select your favorite cuisines', expanded=True):
-#     for i, cuisine_text in enumerate(cuisines_api):
-#         st.session_state.cuisine_list[i] = st.checkbox(label=f'{cuisine_text}', key=i, value=st.session_state.cuisine_list[i])
 
 # Spoonacular API-URL
 api_url = "https://api.spoonacular.com/recipes/findByIngredients"
@@ -139,38 +115,33 @@ if submit_button:
         for recipe in recipes:
             st.write(recipe['title'])
 
-        # #API-Abfrage senden
-        # response = requests.get(api_url, params=parameter)
-        # data = response.json()
-
-        #Rezeptvorschläge 
-        st.header("Look what we've found for you")
-        for recipe in recipes:
-            st.subheader(recipe['title'])
-            st.image(recipe['image'])
-            st.image(recipe['image'])
-            st.write(f"Used ingredients: {', '.join([ingredient['name'] for ingredient in recipe['usedIngredients']])}")
-            st.write(f"Missing ingredients: {', '.join([ingredient['name'] for ingredient in recipe['missedIngredients']])}")
-            st.write(f"Number of missing ingredients: {recipe['missedIngredientCount']}")
-            st.write(f"Number of used ingredients: {recipe['usedIngredientCount']}")
+#Rezeptvorschläge 
+st.header("Look what we've found for you")
+for recipe in recipes:
+    st.subheader(recipe['title'])
+    st.image(recipe['image'])
+    st.image(recipe['image'])
+    st.write(f"Used ingredients: {', '.join([ingredient['name'] for ingredient in recipe['usedIngredients']])}")
+    st.write(f"Missing ingredients: {', '.join([ingredient['name'] for ingredient in recipe['missedIngredients']])}")
+    st.write(f"Number of missing ingredients: {recipe['missedIngredientCount']}")
+    st.write(f"Number of used ingredients: {recipe['usedIngredientCount']}")
   
-
-            #Spoonacular-API für Rezeptinformationen (https://spoonacular.com/food-api/docs#Get-Recipe-Information) / Key ist derselbe
-            api_informations_url = "https://api.spoonacular.com/recipes/{id}/information"
+#Spoonacular-API für Rezeptinformationen (https://spoonacular.com/food-api/docs#Get-Recipe-Information) / Key ist derselbe
+api_informations_url = "https://api.spoonacular.com/recipes/{id}/information"
             
-            if 'id' in recipe:  #API prüfen, ob Zubereitungschritte verfügubar 
-                recipe_id = recipe['id']
-                instructions_url = f"https://api.spoonacular.com/recipes/{recipe_id}/information"
-                instructions_response = requests.get(instructions_url, params={'apiKey': api_key})
-                instructions_data = instructions_response.json()
+if 'id' in recipe:  #API prüfen, ob Zubereitungschritte verfügubar 
+    recipe_id = recipe['id']
+    instructions_url = f"https://api.spoonacular.com/recipes/{recipe_id}/information"
+    instructions_response = requests.get(instructions_url, params={'apiKey': api_key})
+    instructions_data = instructions_response.json()
 
-                if 'instructions' in instructions_data:
-                    st.subheader("Instructions:")
-                    instructions = instructions_data['instructions'].split('\n')  
-                    for step in instructions:
-                        st.write(f"- {step}")  
-                else:
-                    st.write("Recipe steps not available.")
+if 'instructions' in instructions_data:
+    st.subheader("Instructions:")
+    instructions = instructions_data['instructions'].split('\n')  
+    for step in instructions:
+        st.write(f"- {step}")  
+else:
+    st.write("Recipe steps not available.")
 
 # Fußzeile der Anwendung
 st.markdown("---")
