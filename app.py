17f066c78692e9e57d8b57ec6e6b154d0b944874
Joël Sieber <162429161+JoelSiebe@users.noreply.github.com>
2024-03-15 19:15:41 +0100

    s

--- app.py
+++ app.py
@@ -7,8 +7,8 @@ import pandas as pd
 css_background = """   
 <style>
 [data-testid="stAppViewContainer"] > .main {
-    background-image: url("https://i.postimg.cc/cJtrkLQw/pexels-mike-murray-5701888.jpg");
-    background-image: url("https://i.postimg.cc/8cZWBsfz/pexels-david-underland-3312276.jpg");
+    # background-image: url("https://i.postimg.cc/cJtrkLQw/pexels-mike-murray-5701888.jpg");
+    background-image: url("https://i.postimg.cc/J7SKF2s7/pexels-bruno-cervera-3062948.jpg");
     background-size: cover;                 #grösse des hintergrundbilds, cover = ganzer container
     background-position: center center;
     background-repeat: no-repeat;
