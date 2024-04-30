9c5023036ca6e57a1109f8a2044e1177d8cde9e3
Joël Sieber <162429161+JoelSiebe@users.noreply.github.com>
2024-04-30 20:20:13 +0200

    s

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 import streamlit as st
 import requests
 
-# Setzen Sie hier Ihren Spoonacular API-Schlüssel ein
+
 API_KEY = "06491aabe3d2435b8b21a749de46b765"
 
 @st.cache
-def get_recipes(difficulty, max_ready_time):
-    url = f"https://api.spoonacular.com/recipes/complexSearch?apiKey={API_KEY}&difficulty={difficulty}&maxReadyTime={max_ready_time}"
+def get_recipes(difficulty):
+    url = f"https://api.spoonacular.com/recipes/complexSearch?apiKey={API_KEY}&difficulty={difficulty}"
     response = requests.get(url)
     return response.json()
 
@@ -14,13 +14,11 @@ def main():
     st.title("Spoonacular Rezeptsuche")
 
     difficulty = st.selectbox("Schwierigkeitsgrad auswählen", ["easy", "medium", "hard"])
-    max_ready_time = st.slider("Maximale Zubereitungszeit in Minuten", 1, 120, 30)
 
     if st.button("Rezepte suchen"):
-        recipes = get_recipes(difficulty, max_ready_time)
+        recipes = get_recipes(difficulty)
         for recipe in recipes["results"]:
             st.write(f"Name: {recipe['title']}")
-            st.write(f"Zubereitungszeit: {recipe['readyInMinutes']} Minuten")
             st.write("---")
 
 if __name__ == "__main__":
