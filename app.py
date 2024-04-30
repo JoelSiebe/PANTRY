4352430789541ea9f64cc4dded74727d6f8da154
Joël Sieber <162429161+JoelSiebe@users.noreply.github.com>
2024-04-30 22:25:35 +0200

    s

--- app.py
+++ app.py
@@ -37,6 +37,28 @@ def get_recipes(query, cuisine, diet, intolerances, difficulty, duration):
     response = requests.get(url)
     return response.json()
 
+# Daten-Visualisierung in Form eines Piecharts (auf Basis der Nährwerten):
+# Funktion, um Infos aus API abzurufen und in data zu speichern
+def get_nutrition_info(recipe_id):
+    api_nutrition_url = f"https://api.spoonacular.com/recipes/{recipe_id}/nutritionWidget.json"
+    response = requests.get(api_nutrition_url, params={'apiKey': api_key})
+    data = response.json() # Antwort in json umwandeln
+
+# Funktion, um die Nährwerte als Float zurückzugeben (ansonsten funtioniert der Chart auf Streamlit nicht)
+    def parse_nutrition_value(value):
+        # Entfernen von Nicht-Zahlen (ungleich isdigit) und Umwandeln
+        clean_value = ''.join([ch for ch in value if ch.isdigit() or ch == '.'])
+        return float(clean_value)
+
+ # Die relevanten Nährwerte (Kohlenhydrate, Protein, Fett) extrahieren
+ # und mittels zuvor definierter Funktion Float umwandeln
+    carbs = parse_nutrition_value(data['carbs'])
+    protein = parse_nutrition_value(data['protein'])
+    fat = parse_nutrition_value(data['fat'])
+
+# Return eines Dictionaries mit den entsprechenden Nährwerten
+    return {'carbs': carbs, 'protein': protein, 'fat': fat}
+
 def main():
     # Zwei Kolonnen als Platzhalter für Eingabefelder (Filteroptionen) erstellen
     with st.form(key='recipe_form'):
@@ -61,9 +83,30 @@ def main():
             recipes = get_recipes(query, cuisine, diet, intolerances, duration, difficulty)
             if 'results' in recipes:
                 for recipe in recipes["results"]:
+                    st.subheader(recipe['title'])
                     st.write(f"Name: {recipe['title']}")
+                    st.image(recipe['image'])
+                    used_ingredients = ', '.join([ing['name'] for ing in recipe['usedIngredients']])
+                    missed_ingredients = ', '.join([ing['name'] for ing in recipe['missedIngredients']])
+                    st.write("Used Ingredients:", used_ingredients) # Gebrauchte und noch erforderliche Zutaten anzeigen
+                    st.write("Missing Ingredients:", missed_ingredients)
                     st.write("---")
 
+                    # Aufrufen der Nährwerte-Funktion
+                    nutrition_info = get_nutrition_info(recipe['id'])
+                    st.write(f"Nährwerte: {nutrition_info}")
+
+# Anzeigen des Piecharts (Konfiguration von Grösse und Darstellung)
+# Quelle für Workaround, um den Piechart kleiner zu machen: https://discuss.streamlit.io/t/cannot-change-matplotlib-figure-size/10295/10 
+                    col1, col2, col3, col4, col5=st.columns([1,1, 2, 1, 1])
+                    with col3:
+                        labels = ['Carbohydrates', 'Protein', 'Fat'] # Beschriftungen
+                        sizes = [nutrition_data['carbs'], nutrition_data['protein'], nutrition_data['fat']] # Anteilige Grösse der Sektoren gem. API
+                        colors = ['#133337', '#cccccc', '#6897bb'] # Benutzerdefinierte Farben
+                        fig, ax = plt.subplots(figsize=(4, 4)) # Erstellen des Diagramms
+                        ax.pie(sizes, labels=labels, colors=colors, autopct='%1.1f%%', startangle=90) # Darstellung
+                        ax.axis('equal')  # "Rund" machen
+                        st.pyplot(fig) # Anzeigen des Diagramms
 #  Spoonacular-API für Zubereitungsschritte der jeweiligen Rezepe (https://spoonacular.com/food-api/docs#Get-Recipe-Information)
                     api_info_url = f"https://api.spoonacular.com/recipes/{recipe['id']}/information"
                     instructions_response = requests.get(api_info_url, params={'apiKey': api_key})
@@ -92,19 +135,6 @@ st.write("© 2024 Pantry Pal - Where Leftovers Meets Deliciousness. All rights r
 
 ###################################################################
 
-# # Funktion zum Abrufen von Rezepten basierend auf Input (Zutaten) und den ausgewählten Filteroptionen
-# def get_recipes(ingredients, cuisine, difficulty, duration, intolerances, diet):
-#     # Parameter, die an API gesendet werden (aus API-Dokumentation)
-#     parameter = {
-#         'query': ingredients, # oder includeIngredients
-#         'cuisine': cuisine,
-#         'difficulty': difficulty,
-#         'maxReadyTime': duration,
-#         'diet': diet,
-#         'number': 2, # Anz. angezeigter Rezepte
-#         'apiKey': api_key,
-#         'addRecipeInformation': True
-#     }
 # # Filteroptionen (https://docs.streamlit.io/library/api-reference/widgets)
 #     if cuisine != "Any":
 #         parameter['cuisine']= cuisine.lower() # Auswählen der versch. Küchen
@@ -127,80 +157,6 @@ st.write("© 2024 Pantry Pal - Where Leftovers Meets Deliciousness. All rights r
 #         parameter["intolerances"] = intolerances.lower()
 
 
-# #API-Abfrage senden
-#     response = requests.get(api_url, params=parameter)
-#     if response.status_code == 200 and response.text:
-#         try:
-#             recipes = response.json()  # In JSON umwandeln
-#         except requests.exceptions.JSONDecodeError:
-#             st.write("Error decoding JSON response. Please check the API.")
-#             recipes = None
-#     else:
-#         st.write("Failed to fetch recipes. Please check the API and try again.")
-#         recipes = None
-
-#    # return response.json() # Rückgabe des Ergebnisses
-
-# # Daten-Visualisierung in Form eines Piecharts (auf Basis der Nährwerten):
-# # Funktion, um Infos aus API abzurufen und in data zu speichern
-# def get_nutrition_info(recipe_id):
-#     api_nutrition_url = f"https://api.spoonacular.com/recipes/{recipe_id}/nutritionWidget.json"
-#     response = requests.get(api_nutrition_url, params={'apiKey': api_key})
-#     data = response.json() # Antwort in json umwandeln
-
-# # Funktion, um die Nährwerte als Float zurückzugeben (ansonsten funtioniert der Chart auf Streamlit nicht)
-#     def parse_nutrition_value(value):
-#         # Entfernen von Nicht-Zahlen (ungleich isdigit) und Umwandeln
-#         clean_value = ''.join([ch for ch in value if ch.isdigit() or ch == '.'])
-#         return float(clean_value)
-
-#  # Die relevanten Nährwerte (Kohlenhydrate, Protein, Fett) extrahieren
-#  # und mittels zuvor definierter Funktion Float umwandeln
-#     carbs = parse_nutrition_value(data['carbs'])
-#     protein = parse_nutrition_value(data['protein'])
-#     fat = parse_nutrition_value(data['fat'])
-
-# # Return eines Dictionaries mit den entsprechenden Nährwerten
-#     return {'carbs': carbs, 'protein': protein, 'fat': fat}
-   
-# # Zwei Kolonnen als Platzhalter für Eingabefelder (Filteroptionen) erstellen
-# with st.form(key='recipe_form'):
-
-#  # Schaltfläche zum Absenden des Formulars
-
-# # Rezepte anzeigen, wenn die Schaltfläche "Show recipes" geklickt wird
-# if submit_button:
-#     if ingredients: # Es müssen Zutaten eingegeben worden sein
-#         recipes = get_recipes(ingredients, cuisine, difficulty, duration, intolerances, diet)
-#         if recipes:  # Wenn es Rezepte ausgibt
-#             for recipe in recipes:
-#                 if 'title' in recipe:
-#                     st.subheader(recipe['title'])  # Rezepttitel anzeigen
-#                 else:
-#                     st.write("No title found for this recipe")
-#                 if 'image' in recipe:
-#                     st.image(recipe['image'])  # Bild des Rezepts anzeigen
-#                 else:
-#                     st.write("No image found for this recipe")
-#                 used_ingredients = ', '.join([ing['name'] for ing in recipe['usedIngredients']])
-#                 missed_ingredients = ', '.join([ing['name'] for ing in recipe['missedIngredients']])
-#                 st.write("Used Ingredients:", used_ingredients) # Gebrauchte und noch erforderliche Zutaten anzeigen
-#                 st.write("Missing Ingredients:", missed_ingredients)
-                
-# # Nährwertinformationen für das ausgewählte Rezept abrufen (um Piechart zu erstellen)
-#                 nutrition_data = get_nutrition_info(recipe['id'])
-
-# # Anzeigen des Piecharts (Konfiguration von Grösse und Darstellung)
-# # Quelle für Workaround, um den Piechart kleiner zu machen: https://discuss.streamlit.io/t/cannot-change-matplotlib-figure-size/10295/10 
-#                 col1, col2, col3, col4, col5=st.columns([1,1, 2, 1, 1])
-#                 with col3:
-#                     labels = ['Carbohydrates', 'Protein', 'Fat'] # Beschriftungen
-#                     sizes = [nutrition_data['carbs'], nutrition_data['protein'], nutrition_data['fat']] # Anteilige Grösse der Sektoren gem. API
-#                     colors = ['#133337', '#cccccc', '#6897bb'] # Benutzerdefinierte Farben
-#                     fig, ax = plt.subplots(figsize=(4, 4)) # Erstellen des Diagramms
-#                     ax.pie(sizes, labels=labels, colors=colors, autopct='%1.1f%%', startangle=90) # Darstellung
-#                     ax.axis('equal')  # "Rund" machen
-#                     st.pyplot(fig) # Anzeigen des Diagramms
 
 
 
