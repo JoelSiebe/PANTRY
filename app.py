43f44d2fe479274c94b64724c7e307617f8d8ddd
Joël Sieber <162429161+JoelSiebe@users.noreply.github.com>
2024-05-01 11:48:43 +0200

    s

--- app.py
+++ app.py
@@ -31,6 +31,8 @@ st.header("🌍 Choose Your Adventure:")
 st.subheader("Got a favorite cuisine? Any dietary restrictions or allergies? Let us know!")
 st.header("🎩 Then let us do the magic 🐇")
 st.subheader("Leave the rest to us. We're about to turn your leftovers into a feast!")
+st.write("")
+st.write("")
 
 # Konfiguration für Spoonacular-API (key)
 api_key = "0aeaf38837284366b29170821a0ed402"
