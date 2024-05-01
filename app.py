e2c2221c10c593e08b8f204adcc18b8fa90a14a0
Joël Sieber <162429161+JoelSiebe@users.noreply.github.com>
2024-05-01 10:02:22 +0200

    s

--- app.py
+++ app.py
@@ -87,15 +87,20 @@ def main():
                     st.write(f"Name: {recipe['title']}")
                     st.image(recipe['image'])
 
-                    # recipe_info_url = f"https://api.spoonacular.com/recipes/{recipe['id']}/information"
-                    # recipe_info_response = requests.get(recipe_info_url, params={'apiKey': api_key})
-                    # recipe_info = recipe_info_response.json()
+                    recipe_info_url = f"https://api.spoonacular.com/recipes/{recipe['id']}/information"
+                    recipe_info_response = requests.get(recipe_info_url, params={'apiKey': api_key})
+                    recipe_info = recipe_info_response.json()
 
-                    # if 'extendedIngredients' in recipe_info:
-                    #     ingredients = ', '.join([ing['name'] for ing in recipe_info['extendedIngredients']])
-                    #     st.write("Ingredients:", ingredients)
-                    # else:
-                    #     st.write("No ingredients found.")   
+                    if 'usedIngredients' in recipe_info:
+                        used_ingredients = ', '.join([ing['name'] for ing in recipe_info['usedIngredients']])
+                        st.write("Used Ingredients:", used_ingredients)
+                    else:
+                        st.write("No used ingredients found.")
+                    if 'missedIngredients' in recipe_info:
+                        missed_ingredients = ', '.join([ing['name'] for ing in recipe_info['missedIngredients']])
+                        st.write("Missing Ingredients:", missed_ingredients)
+                    else:
+                        st.write("No missing ingredients found")    
                     st.write("---")
                     
         #             # Aufrufen der Nährwerte-Funktion
